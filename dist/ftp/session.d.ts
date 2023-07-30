0fd7173b4ac4bfca852c662761f59292cd19a6f8
Dorian VUOLO <d.vuolo.ext@quadient.com>
2023-07-30 09:49:06 +0200

    init2

--- dist/ftp/session.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="node" />
-import * as net from 'net';
-import { FtpConfiguration } from '../server';
-export declare function bindSession(socket: net.Socket, configuration: FtpConfiguration): void;
--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { create } from './mockfs';
-import { createMockFtpServer, FtpConfiguration } from './server';
-export { create as createMockFilesystem, createMockFtpServer, FtpConfiguration, };
